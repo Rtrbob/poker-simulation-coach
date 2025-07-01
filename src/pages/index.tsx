03dd00cdad56e102a0b2876304530b01739664ea
Robert <R_bachicha@msn.com>
2025-07-01 00:04:59 -0600

    Fix TypeScript homepage placeholder

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Poker Simulation Coach</h1>
+      <p>Welcome! This is where your hand analyzer will appear soon.</p>
+    </div>
+  );
+};
+
+export default Home;
